99b4629f15cea32f2f9ac815c98d9d3f318c2056
Asset Kussainov <asetius2008@gmail.com>
2023-02-18 17:19:23 +0600

    added headless driver method to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,6 +9,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="function")
 def driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="function")
+def headless_driver():
+    opt = Options()
+    opt.add_argument("--headless")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=opt)
     driver.maximize_window()
     yield driver
     driver.quit()
