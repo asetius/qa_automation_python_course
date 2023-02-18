928955c23f841eb25e0d8c7d9a9555844fc4c683
Asset Kussainov <asetius2008@gmail.com>
2023-02-18 17:20:19 +0600

    test headless mode running

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -3,8 +3,8 @@ from pages.elements_page import TextBoxPage
 
 class TestElements:
     class TestTextBox:
-        def test_text_box(self, driver):
-            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+        def test_text_box(self, headless_driver):
+            text_box_page = TextBoxPage(headless_driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
