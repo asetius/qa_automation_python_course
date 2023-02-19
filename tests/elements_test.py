45d63ccc5ab302f47d9b97c5aa4540d4a9211bcc
Asset Kussainov <asetius2008@gmail.com>
2023-02-19 19:47:08 +0600

    added class TestCheckBox in elements_test.py

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,10 +1,10 @@
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
     class TestTextBox:
-        def test_text_box(self, headless_driver):
-            text_box_page = TextBoxPage(headless_driver, 'https://demoqa.com/text-box')
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
@@ -12,3 +12,10 @@ class TestElements:
             assert email == output_email, "the email does not match"
             assert current_address == output_cur_addr, "the current address does not match"
             assert permanent_address == output_per_addr, "the permanent address does not match"
+
+    class TextCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
